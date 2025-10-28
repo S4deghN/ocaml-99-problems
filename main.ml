78f42b9725f7fd8153a895b385499131ecd10975
Zero <sadegh.nobakhti@gmail.com>
2025-10-28 17:18:26 +0330

    Add solutions until 15

--- main.ml
+++ main.ml
@@ -58,6 +58,21 @@ let rev (xs: 'a list): 'a list =
 let is_palindrome (xs: 'a list): bool =
     rev xs = xs
 
+(* Problem 07 *)
+type 'a node =
+    | One of 'a
+    | Many of 'a node list
+
+let flatten (xs: 'a node list): 'a list =
+    let rec flatten_impl (xs: 'a node list) (result: 'a list): 'a list =
+        match xs with
+        | [] -> result
+        | One x :: xs -> flatten_impl xs (x :: result)
+        | Many x :: xs -> flatten_impl xs (flatten_impl x result)
+    in
+    rev (flatten_impl xs [])
+
+
 (* Problem 08 *)
 let rec compress (xs: string list): string list =
     match xs with
@@ -74,6 +89,99 @@ let pack (xs: string list): string list list =
         | x :: rest, _ -> pack_impl rest [x]
     in
     pack_impl xs []
+
+let rec filter (f: 'a -> bool) (xs: 'a list): 'a list =
+    match xs with
+    | [] -> []
+    | x :: rest -> if f x then x :: filter f rest
+                   else filter f rest
+;;
+
+
+(* Problem 10 *)
+let encode (xs: 'a list): (int * 'a) list =
+    let rec encode_impl (xs: 'a list) (count: int): (int * 'a) list =
+        match xs with
+        | x :: (y :: _ as rest) -> if x = y then encode_impl rest (count+1)
+                                   else (count, x) :: encode_impl rest 1
+        | [x] -> (count, x) :: encode_impl [] count
+        | [] -> []
+    in
+    encode_impl xs 1
+
+(* Problem 11 *)
+type 'a rle =
+    | One of 'a
+    | Many of int * 'a
+
+let encode2 (xs: 'a list): 'a rle list =
+    let rec encode_impl (xs: 'a list) (count: int): 'a rle list =
+        match xs with
+        | x :: (y :: _ as rest) -> if x = y then encode_impl rest (count+1)
+                                   else if count > 1 then Many (count, x) :: encode_impl rest 1
+                                   else One x :: encode_impl rest 1
+        | [x] -> if count > 1 then Many (count, x) :: encode_impl [] count
+                 else One x :: encode_impl [] count
+        | [] -> []
+    in
+    encode_impl xs 1
+
+let encode3 (xs: 'a list): 'a rle list =
+    let close (x: 'a) (count: int): 'a rle =
+        if count > 1 then Many (count, x)
+        else One x
+    in
+    let rec encode_impl (xs: 'a list) (count: int): 'a rle list =
+        match xs with
+        | [] -> []
+        | [x] -> close x (count+1) :: encode_impl [] 0
+        | x :: (y :: _ as rest) -> if x = y then encode_impl rest (count+1)
+                       else close x (count+1) :: encode_impl rest 0
+    in
+    encode_impl xs 0
+
+(* Problem 12 *)
+let rec decode (xs: 'a rle list): 'a list =
+    let rec expand acc n x =
+        if n > 0 then expand (x :: acc) (n-1) x
+        else acc
+    in
+    match xs with
+    | [] -> []
+    | One x :: rest -> x :: decode rest
+    | Many (count, x) :: rest -> expand [] count x @ decode rest
+
+(* Problem 13 *)
+let duplicate (xs: 'a list): 'a list =
+    let rec dup acc = function
+        | [] -> acc
+        | x :: t -> dup (x :: x :: acc) t
+    in
+    rev (dup [] xs)
+
+(* Problem 14 *)
+let replicate (xs: 'a list) (n: int): 'a list =
+    let rec expand acc x n =
+        if n > 0 then  expand (x :: acc) x (n-1)
+        else acc
+    in
+    let rec rep acc xs n =
+        match xs with
+        | [] -> acc
+        | x :: t -> rep ((expand [] x n) @ acc) t n
+    in
+    rev (rep [] xs n)
+
+(* Problem 15 *)
+let drop (xs: 'a list) (n'th: int): 'a list =
+    let rec drp acc cnt xs =
+        match xs with
+        | [] -> acc
+        | x :: rest -> if (cnt+1) = n'th then drp acc 0 rest
+                       else  drp (x :: acc) (cnt+1) rest
+    in
+    rev (drp [] 0 xs)
+
 
 let () =
     print_endline "Hello, world!"
